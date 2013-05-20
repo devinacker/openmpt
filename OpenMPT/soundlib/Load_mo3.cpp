devinacker/openmpt

commit e15d487c5dd5d19d68f7a7b4d750ddcbfe8bf4ea

[Ref] Fix some trivial unused variable warnings.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk@2128 56274372-70c3-4bfc-bfc3-4c3a0b034d27

OpenMPT/soundlib/Load_mo3.cpp
@@ -20,8 +20,6 @@ bool CSoundFile::ReadMO3(FileReader &file, ModLoadingFlags loadFlags)
 //-------------------------------------------------------------------
 {
 	file.Rewind();
-	const void *stream = file.GetRawData();
-	int length = file.GetLength();
 
 	// No valid MO3 file (magic bytes: "MO3")
 	if(!file.CanRead(8) || !file.ReadMagic("MO3"))
@@ -45,6 +43,9 @@ bool CSoundFile::ReadMO3(FileReader &file, ModLoadingFlags loadFlags)
 	return false;
 
 #else
+
+	const void *stream = file.GetRawData();
+	int length = file.GetLength();
 
 	bool result = false;	// Result of trying to load the module, false == fail.
 

OpenMPT/soundlib/Load_mid.cpp
@@ -1201,7 +1201,7 @@ bool CSoundFile::ReadMID(const BYTE *lpStream, DWORD dwMemLength, ModLoadingFlag
 
 #else // !MODPLUG_TRACKER
 
-bool CSoundFile::ReadMID(const BYTE *lpStream, DWORD dwMemLength, ModLoadingFlags loadFlags)
+bool CSoundFile::ReadMID(const BYTE * /*lpStream*/, DWORD /*dwMemLength*/, ModLoadingFlags /*loadFlags*/)
 {
 	return false;
 }

commit 0e592a43c813dd3495387e421f9fc10f0712f623

[Fix] Reading mo3 files broke in r2128. Repair.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk@2142 56274372-70c3-4bfc-bfc3-4c3a0b034d27

OpenMPT/soundlib/Load_mo3.cpp
@@ -44,6 +44,7 @@ bool CSoundFile::ReadMO3(FileReader &file, ModLoadingFlags loadFlags)
 
 #else
 
+	file.Rewind();
 	const void *stream = file.GetRawData();
 	int length = file.GetLength();
 
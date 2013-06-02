devinacker/openmpt

commit e68158003385465ccb51bc28e1d3cd66ad603661

[Ref] Add a simple int24 type to typedefs.h. Useful for reading or writing 24bit integer audio data. [Ref] Optionally support non-interleaved output in CSoundFile::Read. [Fix] Fix openmpt::module::read when rendering some GB of audio data at once on 64bit architectures. [Fix] Avoid allocating memory in render path in openmpt::module::read by using the new non-interleaved output of CSoundFile::Read.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk@2277 56274372-70c3-4bfc-bfc3-4c3a0b034d27

OpenMPT/soundlib/Sndmix.cpp
@@ -40,6 +40,28 @@ extern DWORD Convert32To16(LPVOID lpBuffer, int *, DWORD nSamples);
 extern DWORD Convert32To24(LPVOID lpBuffer, int *, DWORD nSamples);
 extern DWORD Convert32To32(LPVOID lpBuffer, int *, DWORD nSamples);
 extern DWORD Convert32ToFloat32(LPVOID lpBuffer, int *pBuffer, DWORD lSampleCount);
+
+extern void Convert32ToNonInterleaved(uint8 * const * const buffers, const int *mixbuffer, std::size_t channels, std::size_t count);
+extern void Convert32ToNonInterleaved(int16 * const * const buffers, const int *mixbuffer, std::size_t channels, std::size_t count);
+extern void Convert32ToNonInterleaved(int24 * const * const buffers, const int *mixbuffer, std::size_t channels, std::size_t count);
+extern void Convert32ToNonInterleaved(int32 * const * const buffers, const int *mixbuffer, std::size_t channels, std::size_t count);
+extern void Convert32ToNonInterleaved(float * const * const buffers, const int *mixbuffer, std::size_t channels, std::size_t count);
+
+template<typename Tsample>
+void Convert32ToNonInterleaved(void * const *outputBuffers, std::size_t offset, const int *mixbuffer, std::size_t channels, std::size_t count)
+{
+	Tsample *buffers[4];
+	MemsetZero(buffers);
+	for(std::size_t channel = 0; channel < channels; ++channel)
+	{
+		buffers[channel] = reinterpret_cast<Tsample*>(outputBuffers[channel]);
+		buffers[channel] += offset; // skip to output position
+	}
+	Convert32ToNonInterleaved(buffers, mixbuffer, channels, count);
+}
+
+
+
 #ifdef ENABLE_X86
 extern VOID X86_Dither(int *pBuffer, UINT nSamples, UINT nBits);
 #endif
@@ -173,8 +195,8 @@ BOOL CSoundFile::GlobalFadeSong(UINT msec)
 }
 
 
-UINT CSoundFile::Read(LPVOID lpDestBuffer, UINT count)
-//----------------------------------------------------
+UINT CSoundFile::Read(LPVOID lpDestBuffer, UINT count, void * const *outputBuffers)
+//---------------------------------------------------------------------------------
 {
 	LPBYTE lpBuffer = (LPBYTE)lpDestBuffer;
 	LPCONVERTPROC pCvt = nullptr;
@@ -182,6 +204,7 @@ UINT CSoundFile::Read(LPVOID lpDestBuffer, UINT count)
 	size_t lSampleSize;
 	UINT nStat = 0;
 	UINT nMaxPlugins;
+	std::size_t renderedCount = 0;
 
 	nMaxPlugins = MAX_MIXPLUGINS;
 	while ((nMaxPlugins > 0) && (!m_MixPlugins[nMaxPlugins-1].pMixPlugin)) nMaxPlugins--;
@@ -200,7 +223,7 @@ UINT CSoundFile::Read(LPVOID lpDestBuffer, UINT count)
 	lSampleSize *= m_MixerSettings.GetBitsPerSample()/8;
 
 	lMax = count;
-	if ((!lMax) || (!lpBuffer) || (!m_nChannels)) return 0;
+	if((!lMax) || ((!lpBuffer) && (!outputBuffers)) || (!m_nChannels)) return 0;
 	samplecount_t lRead = lMax;
 	if(m_SongFlags[SONG_ENDREACHED])
 		goto MixDone;
@@ -357,6 +380,8 @@ UINT CSoundFile::Read(LPVOID lpDestBuffer, UINT count)
 		}
 #endif
 
+		if(lpBuffer) // old style interleaved output buffer
+		{
 		#ifndef MODPLUG_TRACKER
 			LPBYTE buf_beg = lpBuffer;
 		#endif
@@ -372,8 +397,34 @@ UINT CSoundFile::Read(LPVOID lpDestBuffer, UINT count)
 				ApplyFinalOutputGainFloat(reinterpret_cast<float*>(buf_beg), reinterpret_cast<float*>(buf_end));
 			}
 		#endif
+		}
+
+		if(outputBuffers) // non-interleaved one output buffer per channel
+		{
+			switch(m_MixerSettings.m_SampleFormat)
+			{
+				case SampleFormatUnsigned8: Convert32ToNonInterleaved<uint8>(outputBuffers, renderedCount, MixSoundBuffer, m_MixerSettings.gnChannels, lCount); break;
+				case SampleFormatInt16:     Convert32ToNonInterleaved<int16>(outputBuffers, renderedCount, MixSoundBuffer, m_MixerSettings.gnChannels, lCount); break;
+				case SampleFormatInt24:     Convert32ToNonInterleaved<int24>(outputBuffers, renderedCount, MixSoundBuffer, m_MixerSettings.gnChannels, lCount); break;
+				case SampleFormatInt32:     Convert32ToNonInterleaved<int32>(outputBuffers, renderedCount, MixSoundBuffer, m_MixerSettings.gnChannels, lCount); break;
+				case SampleFormatFloat32:   Convert32ToNonInterleaved<float>(outputBuffers, renderedCount, MixSoundBuffer, m_MixerSettings.gnChannels, lCount); break;
+				default:
+					break;
+			}
+			#ifndef MODPLUG_TRACKER
+				if(m_MixerSettings.IsFloatSampleFormat())
+				{
+					// Apply final output gain for floating point output after conversion so we do not suffer underflow or clipping
+					for(std::size_t channel = 0; channel < m_MixerSettings.gnChannels; ++channel)
+					{
+						ApplyFinalOutputGainFloat(reinterpret_cast<float*>(outputBuffers[channel]) + renderedCount, reinterpret_cast<float*>(outputBuffers[channel]) + renderedCount + lCount);
+					}
+				}
+			#endif // !MODPLUG_TRACKER
+		}
 
 		// Buffer ready
+		renderedCount += lCount;
 		lRead -= lCount;
 		m_nBufferCount -= lCount;
 		m_lTotalSampleCount += lCount;		// increase sample count for VSTTimeInfo.
@@ -381,7 +432,7 @@ UINT CSoundFile::Read(LPVOID lpDestBuffer, UINT count)
 MixDone:
 	if (lRead) memset(lpBuffer, (m_MixerSettings.m_SampleFormat == SampleFormatUnsigned8) ? 0x80 : 0, lRead * lSampleSize);
 	if (nStat) { m_nMixStat += nStat-1; m_nMixStat /= nStat; }
-	return lMax - lRead;
+	return renderedCount;
 }
 
 

OpenMPT/common/typedefs.h
@@ -208,6 +208,33 @@ const uint64 uint64_max = UINT64_MAX;
 
 #endif
 
+struct int24
+{
+	uint8 bytes[3];
+	int24() { bytes[0] = bytes[1] = bytes[2] = 0; }
+	explicit int24(int other)
+	{
+		#ifdef PLATFORM_BIG_ENDIAN
+			bytes[0] = ((unsigned int)other>>16)&0xff;
+			bytes[1] = ((unsigned int)other>> 8)&0xff;
+			bytes[2] = ((unsigned int)other>> 0)&0xff;
+		#else
+			bytes[0] = ((unsigned int)other>> 0)&0xff;
+			bytes[1] = ((unsigned int)other>> 8)&0xff;
+			bytes[2] = ((unsigned int)other>>16)&0xff;
+		#endif
+	}
+	operator int() const
+	{
+		#ifdef PLATFORM_BIG_ENDIAN
+			return ((int8)bytes[0] * 65536) + (bytes[1] * 256) + bytes[2];
+		#else
+			return ((int8)bytes[2] * 65536) + (bytes[1] * 256) + bytes[0];
+		#endif
+	}
+};
+STATIC_ASSERT(sizeof(int24) == 3);
+
 typedef float float32;
 STATIC_ASSERT(sizeof(float32) == 4);
 

OpenMPT/libopenmpt/libopenmpt_impl.cpp
@@ -141,8 +141,6 @@ void module_impl::apply_libopenmpt_defaults() {
 	set_render_param( module::RENDER_QUALITY_PERCENT, 100 );
 }
 void module_impl::init() {
-	m_int16Buffer.resize( 4096 * 4 );
-	m_floatBuffer.resize( 4096 * 4 );
 	m_sndFile = std::unique_ptr<CSoundFile>(new CSoundFile());
 	m_LogForwarder = std::unique_ptr<log_forwarder>(new log_forwarder(m_Log));
 	m_sndFile->SetCustomLog( m_LogForwarder.get() );
@@ -164,10 +162,32 @@ void module_impl::load( const FileReader & file ) {
 		m_loaderMessages.push_back( LogLevelToString( i->first ) + std::string(": ") + i->second );
 	}
 }
-std::size_t module_impl::read_wrapper( void * buffer, std::size_t count ) {
+std::size_t module_impl::read_wrapper( std::size_t count, std::int16_t * left, std::int16_t * right, std::int16_t * rear_left, std::int16_t * rear_right ) {
 	std::size_t count_read = 0;
 	while ( count > 0 ) {
-		std::size_t count_chunk = m_sndFile->Read( buffer, static_cast<UINT>( std::min<std::size_t>( count, std::numeric_limits<UINT>::max() / 2 / 4 / 4 ) ) ); // safety margin / samplesize / channels
+		std::int16_t * const buffers[4] = { left + count_read, right + count_read, rear_left + count_read, rear_right + count_read };
+		std::size_t count_chunk = m_sndFile->Read(
+			0,
+			static_cast<UINT>( std::min<std::size_t>( count, std::numeric_limits<UINT>::max() / 2 / 4 / 4 ) ), // safety margin / samplesize / channels
+			reinterpret_cast<void*const*>( buffers )
+			);
+		if ( count_chunk == 0 ) {
+			break;
+		}
+		count -= count_chunk;
+		count_read += count_chunk;
+	}
+	return count_read;
+}
+std::size_t module_impl::read_wrapper( std::size_t count, float * left, float * right, float * rear_left, float * rear_right ) {
+	std::size_t count_read = 0;
+	while ( count > 0 ) {
+		float * const buffers[4] = { left + count_read, right + count_read, rear_left + count_read, rear_right + count_read };
+		std::size_t count_chunk = m_sndFile->Read(
+			0,
+			static_cast<UINT>( std::min<std::size_t>( count, std::numeric_limits<UINT>::max() / 2 / 4 / 4 ) ), // safety margin / samplesize / channels
+			reinterpret_cast<void*const*>( buffers )
+			);
 		if ( count_chunk == 0 ) {
 			break;
 		}
@@ -382,11 +402,7 @@ std::size_t module_impl::read( std::int32_t samplerate, std::size_t count, std::
 		throw openmpt::exception_message("null pointer");
 	}
 	apply_mixer_settings( samplerate, 1, false );
-	m_int16Buffer.resize( count * 1 );
-	count = read_wrapper( &m_int16Buffer[0], count );
-	for ( std::size_t i = 0; i < count; ++i ) {
-		mono[i] = m_int16Buffer[i*1];
-	}
+	count = read_wrapper( count, mono, 0, 0, 0 );
 	m_currentPositionSeconds += static_cast<double>( count ) / static_cast<double>( samplerate );
 	return count;
 }
@@ -395,12 +411,7 @@ std::size_t module_impl::read( std::int32_t samplerate, std::size_t count, std::
 		throw openmpt::exception_message("null pointer");
 	}
 	apply_mixer_settings( samplerate, 2, false );
-	m_int16Buffer.resize( count * 2 );
-	count = read_wrapper( &m_int16Buffer[0], count );
-	for ( std::size_t i = 0; i < count; ++i ) {
-		left[i] = m_int16Buffer[i*2+0];
-		right[i] = m_int16Buffer[i*2+1];
-	}
+	count = read_wrapper( count, left, right, 0, 0 );
 	m_currentPositionSeconds += static_cast<double>( count ) / static_cast<double>( samplerate );
 	return count;
 }
@@ -409,14 +420,7 @@ std::size_t module_impl::read( std::int32_t samplerate, std::size_t count, std::
 		throw openmpt::exception_message("null pointer");
 	}
 	apply_mixer_settings( samplerate, 4, false );
-	m_int16Buffer.resize( count * 4 );
-	count = read_wrapper( &m_int16Buffer[0], count );
-	for ( std::size_t i = 0; i < count; ++i ) {
-		left[i] = m_int16Buffer[i*4+0];
-		right[i] = m_int16Buffer[i*4+1];
-		rear_left[i] = m_int16Buffer[i*4+2];
-		rear_right[i] = m_int16Buffer[i*4+3];
-	}
+	count = read_wrapper( count, left, right, rear_left, rear_right );
 	m_currentPositionSeconds += static_cast<double>( count ) / static_cast<double>( samplerate );
 	return count;
 }
@@ -425,11 +429,7 @@ std::size_t module_impl::read( std::int32_t samplerate, std::size_t count, float
 		throw openmpt::exception_message("null pointer");
 	}
 	apply_mixer_settings( samplerate, 1, true );
-	m_floatBuffer.resize( count * 1 );
-	count = read_wrapper( &m_floatBuffer[0], count );
-	for ( std::size_t i = 0; i < count; ++i ) {
-		mono[i] = m_floatBuffer[i*1];
-	}
+	count = read_wrapper( count, mono, 0, 0, 0 );
 	m_currentPositionSeconds += static_cast<double>( count ) / static_cast<double>( samplerate );
 	return count;
 }
@@ -438,12 +438,7 @@ std::size_t module_impl::read( std::int32_t samplerate, std::size_t count, float
 		throw openmpt::exception_message("null pointer");
 	}
 	apply_mixer_settings( samplerate, 2, true );
-	m_floatBuffer.resize( count * 2 );
-	count = read_wrapper( &m_floatBuffer[0], count );
-	for ( std::size_t i = 0; i < count; ++i ) {
-		left[i] = m_floatBuffer[i*2+0];
-		right[i] = m_floatBuffer[i*2+1];
-	}
+	count = read_wrapper( count, left, right, 0, 0 );
 	m_currentPositionSeconds += static_cast<double>( count ) / static_cast<double>( samplerate );
 	return count;
 }
@@ -452,14 +447,7 @@ std::size_t module_impl::read( std::int32_t samplerate, std::size_t count, float
 		throw openmpt::exception_message("null pointer");
 	}
 	apply_mixer_settings( samplerate, 4, true );
-	m_floatBuffer.resize( count * 4 );
-	count = read_wrapper( &m_floatBuffer[0], count );
-	for ( std::size_t i = 0; i < count; ++i ) {
-		left[i] = m_floatBuffer[i*4+0];
-		right[i] = m_floatBuffer[i*4+1];
-		rear_left[i] = m_floatBuffer[i*4+2];
-		rear_right[i] = m_floatBuffer[i*4+3];
-	}
+	count = read_wrapper( count, left, right, rear_left, rear_right );
 	m_currentPositionSeconds += static_cast<double>( count ) / static_cast<double>( samplerate );
 	return count;
 }

OpenMPT/libopenmpt/libopenmpt_impl.hpp
@@ -65,8 +65,6 @@ class module_impl {
 protected:
 	std::shared_ptr<log_interface> m_Log;
 	std::unique_ptr<log_forwarder> m_LogForwarder;
-	std::vector<std::int16_t> m_int16Buffer;
-	std::vector<float> m_floatBuffer;
 	double m_currentPositionSeconds;
 	std::unique_ptr<CSoundFile> m_sndFile;
 	std::vector<std::string> m_loaderMessages;
@@ -81,7 +79,8 @@ private:
 	void init();
 	static void load( CSoundFile & sndFile, const FileReader & file );
 	void load( const FileReader & file );
-	std::size_t read_wrapper( void * buffer, std::size_t count );
+	std::size_t read_wrapper( std::size_t count, std::int16_t * left, std::int16_t * right, std::int16_t * rear_left, std::int16_t * rear_right );
+	std::size_t read_wrapper( std::size_t count, float * left, float * right, float * rear_left, float * rear_right );
 public:
 	static std::vector<std::string> get_supported_extensions();
 	static bool is_extension_supported( const std::string & extension );

OpenMPT/soundlib/Fastmix.cpp
@@ -1796,6 +1796,22 @@ DWORD Convert32To8(LPVOID lp16, int *pBuffer, DWORD lSampleCount)
 	#endif
 }
 
+void Convert32ToNonInterleaved(uint8 * const * const buffers, const int *mixbuffer, std::size_t channels, std::size_t count)
+//--------------------------------------------------------------------------------------------------------------------------
+{
+	for(std::size_t i = 0; i < count; ++i)
+	{
+		for(std::size_t channel = 0; channel < channels; ++channel)
+		{
+			int v = *mixbuffer;
+			if(v < MIXING_CLIPMIN) v = MIXING_CLIPMIN;
+			else if(v > MIXING_CLIPMAX) v = MIXING_CLIPMAX;
+			buffers[channel][i] = (uint8)((v >> (24-MIXING_ATTENUATION))+0x80); // unsigned
+			mixbuffer++;
+		}
+	}
+}
+
 
 #ifdef ENABLE_X86
 static DWORD X86_Convert32To16(LPVOID lp16, int *pBuffer, DWORD lSampleCount)
@@ -1861,6 +1877,19 @@ DWORD Convert32To16(LPVOID lp16, int *pBuffer, DWORD lSampleCount)
 	#endif
 }
 
+void Convert32ToNonInterleaved(int16 * const * const buffers, const int *mixbuffer, std::size_t channels, std::size_t count)
+//--------------------------------------------------------------------------------------------------------------------------
+{
+	for(std::size_t i = 0; i < count; ++i)
+	{
+		for(std::size_t channel = 0; channel < channels; ++channel)
+		{
+			buffers[channel][i] = (int16)(Clamp(*mixbuffer, MIXING_CLIPMIN, MIXING_CLIPMAX) >> (16-MIXING_ATTENUATION));
+			mixbuffer++;
+		}
+	}
+}
+
 
 #ifdef ENABLE_X86
 static DWORD X86_Convert32To24(LPVOID lp16, int *pBuffer, DWORD lSampleCount)
@@ -1906,22 +1935,14 @@ done:
 static DWORD C_Convert32To24(LPVOID lp16, int *pBuffer, DWORD lSampleCount)
 //-------------------------------------------------------------------------
 {
-	uint8 * p = (uint8*)lp16;
+	int24 * p = (int24*)lp16;
 	for(DWORD i=0; i<lSampleCount; i++)
 	{
 		int v = pBuffer[i];
 		if(v < MIXING_CLIPMIN) v = MIXING_CLIPMIN;
 		else if(v > MIXING_CLIPMAX) v = MIXING_CLIPMAX;
 		v >>= (8-MIXING_ATTENUATION);
-#ifndef PLATFORM_BIG_ENDIAN
-		p[i*3+0] = ((unsigned)v>>0)&0xff;
-		p[i*3+1] = ((unsigned)v>>8)&0xff;
-		p[i*3+2] = ((unsigned)v>>16)&0xff;
-#else
-		p[i*3+0] = ((unsigned)v>>16)&0xff;
-		p[i*3+1] = ((unsigned)v>>8)&0xff;
-		p[i*3+2] = ((unsigned)v>>0)&0xff;
-#endif
+		p[i] = (int24)v;
 	}
 	return lSampleCount * 3;
 }
@@ -1936,6 +1957,18 @@ DWORD Convert32To24(LPVOID lp16, int *pBuffer, DWORD lSampleCount)
 	#endif
 }
 
+void Convert32ToNonInterleaved(int24 * const * const buffers, const int *mixbuffer, std::size_t channels, std::size_t count)
+//--------------------------------------------------------------------------------------------------------------------------
+{
+	for(std::size_t i = 0; i < count; ++i)
+	{
+		for(std::size_t channel = 0; channel < channels; ++channel)
+		{
+			buffers[channel][i] = (int24)(Clamp(*mixbuffer, MIXING_CLIPMIN, MIXING_CLIPMAX) >> (8-MIXING_ATTENUATION));
+			mixbuffer++;
+		}
+	}
+}
 
 
 #ifdef ENABLE_X86
@@ -2001,6 +2034,19 @@ DWORD Convert32To32(LPVOID lp16, int *pBuffer, DWORD lSampleCount)
 	#endif
 }
 
+void Convert32ToNonInterleaved(int32 * const * const buffers, const int *mixbuffer, std::size_t channels, std::size_t count)
+//--------------------------------------------------------------------------------------------------------------------------
+{
+	for(std::size_t i = 0; i < count; ++i)
+	{
+		for(std::size_t channel = 0; channel < channels; ++channel)
+		{
+			buffers[channel][i] = (int32)(Clamp(*mixbuffer, MIXING_CLIPMIN, MIXING_CLIPMAX) << MIXING_ATTENUATION);
+			mixbuffer++;
+		}
+	}
+}
+
 
 // convert to 32 bit floats and do NOT clip to [-1,1]
 DWORD Convert32ToFloat32(LPVOID lpBuffer, int *pBuffer, DWORD lSampleCount)
@@ -2013,6 +2059,20 @@ DWORD Convert32ToFloat32(LPVOID lpBuffer, int *pBuffer, DWORD lSampleCount)
 		out[i] = pBuffer[i] * factor;
 	}
 	return lSampleCount * 4;
+}
+
+void Convert32ToNonInterleaved(float * const * const buffers, const int *mixbuffer, std::size_t channels, std::size_t count)
+//--------------------------------------------------------------------------------------------------------------------------
+{
+	const float factor = (1.0f/(float)MIXING_CLIPMAX);
+	for(std::size_t i = 0; i < count; ++i)
+	{
+		for(std::size_t channel = 0; channel < channels; ++channel)
+		{
+			buffers[channel][i] = *mixbuffer * factor;
+			mixbuffer++;
+		}
+	}
 }
 
 

OpenMPT/soundlib/Sndfile.h
@@ -590,7 +590,7 @@ public:
 	void StopAllVsti();    //rewbs.VSTCompliance
 	void RecalculateGainForAllPlugs();
 	void ResetChannels();
-	UINT Read(LPVOID lpBuffer, UINT cbBuffer);
+	UINT Read(LPVOID lpBuffer, UINT cbBuffer, void * const *outputBuffers = nullptr);
 	UINT CreateStereoMix(int count);
 	BOOL FadeSong(UINT msec);
 	BOOL GlobalFadeSong(UINT msec);

commit 09a6d11de34d1a8cb81c2b4d2078242452f3b9c8

[Fix] Fix crash at end of song in libopenmpt: Do not try to clear remaining lpBuffer if it is a null pointer. (broken since r2277)
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk@2290 56274372-70c3-4bfc-bfc3-4c3a0b034d27

OpenMPT/soundlib/Sndmix.cpp
@@ -430,7 +430,7 @@ UINT CSoundFile::Read(LPVOID lpDestBuffer, UINT count, void * const *outputBuffe
 		m_lTotalSampleCount += lCount;		// increase sample count for VSTTimeInfo.
 	}
 MixDone:
-	if (lRead) memset(lpBuffer, (m_MixerSettings.m_SampleFormat == SampleFormatUnsigned8) ? 0x80 : 0, lRead * lSampleSize);
+	if (lRead && lpBuffer) memset(lpBuffer, (m_MixerSettings.m_SampleFormat == SampleFormatUnsigned8) ? 0x80 : 0, lRead * lSampleSize); // clear remaining interleaved output buffer
 	if (nStat) { m_nMixStat += nStat-1; m_nMixStat /= nStat; }
 	return renderedCount;
 }
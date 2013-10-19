devinacker/openmpt

commit 9d4cf22c07292c4083e880e363ef3d2a31affb4d

[Ref] Moved song message functionality to own class SongMessage. Use std::string as an implementation base.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk@1866 56274372-70c3-4bfc-bfc3-4c3a0b034d27

OpenMPT/soundlib/Sndfile.cpp
@@ -16,9 +16,10 @@
 #endif // MODPLUG_TRACKER
 #include "../common/version.h"
 #include "../common/serialization_utils.h"
-#include "sndfile.h"
+#include "Sndfile.h"
 #include "tuningcollection.h"
 #include "../common/StringFixer.h"
+#include "FileReader.h"
 
 #ifndef NO_COPYRIGHT
 #ifndef NO_MMCMP_SUPPORT
@@ -432,7 +433,6 @@ CSoundFile::CSoundFile() :
 	m_nMixChannels = 0;
 	m_nSamples = 0;
 	m_nInstruments = 0;
-	m_lpszSongComments = nullptr;
 	m_nFreqFactor = m_nTempoFactor = 128;
 	m_nMinPeriod = MIN_PERIOD;
 	m_nMaxPeriod = 0x7FFF;
@@ -529,7 +529,6 @@ BOOL CSoundFile::Create(LPCBYTE lpStream, void *pModDoc, DWORD dwMemLength)
 	m_nSamplePreAmp = 48;
 	m_nVSTiVolume = 48;
 	m_nMaxOrderPosition = 0;
-	m_lpszSongComments = nullptr;
 	m_nMixLevels = mixLevels_compatible;	// Will be overridden if appropriate.
 	MemsetZero(ChnMix);
 	MemsetZero(Instruments);
@@ -538,6 +537,7 @@ BOOL CSoundFile::Create(LPCBYTE lpStream, void *pModDoc, DWORD dwMemLength)
 	//Order.assign(MAX_ORDERS, Order.GetInvalidPatIndex());
 	Order.resize(1);
 	Patterns.ClearPatterns();
+	songMessage.clear();
 
 	for (CHANNELINDEX nChn = 0; nChn < MAX_BASECHANNELS; nChn++)
 	{
@@ -647,9 +647,9 @@ BOOL CSoundFile::Create(LPCBYTE lpStream, void *pModDoc, DWORD dwMemLength)
 
 #ifdef ZIPPED_MOD_SUPPORT
 		// Read archive comment if there is no song comment
-		if((!m_lpszSongComments) && unzip.GetComments(false))
+		if(!songMessage.empty() && unzip.GetComments(false))
 		{
-			m_lpszSongComments = (LPSTR)unzip.GetComments(true);
+			songMessage.assign(unzip.GetComments(true));
 		}
 #endif
 #ifdef MMCMP_SUPPORT
@@ -835,7 +835,7 @@ BOOL CSoundFile::Destroy()
 
 	Patterns.DestroyPatterns();
 
-	FreeMessage();
+	songMessage.clear();
 
 	for(SAMPLEINDEX i = 1; i < MAX_SAMPLES; i++)
 	{

OpenMPT/mptrack/AbstractVstEditor.cpp
@@ -19,6 +19,7 @@
 #include "../common/StringFixer.h"
 #include "MIDIMacros.h"
 #include "VstPresets.h"
+#include "../soundlib/FileReader.h"
 
 #ifndef NO_VST
 

OpenMPT/mptrack/CleanupSong.cpp
@@ -14,6 +14,7 @@
 #include "Mainfrm.h"
 #include "modsmp_ctrl.h"
 #include "CleanupSong.h"
+#include "../common/StringFixer.h"
 
 // Default checkbox state
 bool CModCleanupDlg::m_bCheckBoxes[CU_MAX_CLEANUP_OPTIONS] =

OpenMPT/mptrack/Ctrl_com.cpp
@@ -127,9 +127,10 @@ void CCtrlComments::UpdateView(DWORD dwHint, CObject *pHint)
 	m_EditComments.SetRedraw(FALSE);
 	m_EditComments.SetSel(0, -1, TRUE);
 	m_EditComments.ReplaceSel("");
-	if (m_pSndFile->m_lpszSongComments)
+	if(!m_pSndFile->songMessage.empty())
 	{
-		CHAR s[256], *p = m_pSndFile->m_lpszSongComments, c;
+		CHAR s[256], c;
+		const char *p = m_pSndFile->songMessage.c_str();
 		UINT ln = 0;
 		while ((c = *p++) != NULL)
 		{
@@ -167,24 +168,23 @@ void CCtrlComments::UpdateView(DWORD dwHint, CObject *pHint)
 void CCtrlComments::OnCommentsChanged()
 //-------------------------------------
 {
-	CHAR s[256], *oldcomments = NULL;
-	
 	if ((m_nLockCount) || (!m_pSndFile)
 		|| !m_pSndFile->GetModSpecifications().hasComments) return;
 	if ((!m_bInitialized) || (!m_EditComments.m_hWnd) || (!m_EditComments.GetModify())) return;
-	if (m_pSndFile->m_lpszSongComments)
-	{
-		oldcomments = m_pSndFile->m_lpszSongComments;
-		m_pSndFile->m_lpszSongComments = NULL;
-	}
+
+	CHAR s[LINE_LENGTH + 2];
+	const char *oldMsg = m_pSndFile->songMessage.c_str();
+
 	// Updating comments
 	{
-
-
 		UINT n = m_EditComments.GetLineCount();
 		LPSTR p = new char[n * LINE_LENGTH + 1];
+		if (!p)
+		{
+			return;
+		}
 		p[0] = 0;
-		if (!p) return;
+
 		for (UINT i=0; i<n; i++)
 		{
 			int ln = m_EditComments.GetLine(i, s, LINE_LENGTH);
@@ -206,27 +206,20 @@ void CCtrlComments::OnCommentsChanged()
 			len--;
 			p[len] = 0;
 		}
-		if (p[0])
-			m_pSndFile->m_lpszSongComments = p;
-		else
-			delete[] p;
-		if (oldcomments)
+
+		m_EditComments.SetModify(FALSE);
+		if(p != m_pSndFile->songMessage)
 		{
-			bool bSame = false;
-			if ((m_pSndFile->m_lpszSongComments)
-			 && (!strcmp(m_pSndFile->m_lpszSongComments, oldcomments))) bSame = true;
-			delete[] oldcomments;
-			if (bSame) return;
-		} else
-		{
-			if (!m_pSndFile->m_lpszSongComments) return;
+			m_pSndFile->songMessage.assign(p);
+
+			if(m_pModDoc)
+			{
+				m_pModDoc->SetModified();
+				m_pModDoc->UpdateAllViews(NULL, HINT_MODCOMMENTS, this);
+			}
 		}
-		if (m_pModDoc)
-		{
-			m_EditComments.SetModify(FALSE);
-			m_pModDoc->SetModified();
-			m_pModDoc->UpdateAllViews(NULL, HINT_MODCOMMENTS, this);
-		}
+
+		delete[] p;
 	}
 }
 

OpenMPT/mptrack/Ctrl_smp.cpp
@@ -27,6 +27,7 @@
 #include "modsmp_ctrl.h"
 #include "Autotune.h"
 #include "../common/StringFixer.h"
+#include "../soundlib/FileReader.h"
 #include <Shlwapi.h>
 
 #ifdef _DEBUG

OpenMPT/mptrack/Mod2wave.cpp
@@ -624,9 +624,9 @@ void CLayer3Convert::OnOK()
 	m_CbnGenre.GetWindowText(sText, sizeof(sText));
 	m_FileTags.genre = sText;
 
-	if (m_pSndFile->m_lpszSongComments)
+	if(!m_pSndFile->songMessage.empty())
 	{
-		m_FileTags.comments = m_pSndFile->GetSongMessage(leLF);
+		m_FileTags.comments = m_pSndFile->songMessage.GetFormatted(SongMessage::leLF);
 	}
 	else
 	{

OpenMPT/mptrack/Modedit.cpp
@@ -16,6 +16,7 @@
 #include "dlsbank.h"
 #include "modsmp_ctrl.h"
 #include "../common/misc_util.h"
+#include "../common/StringFixer.h"
 
 #pragma warning(disable:4244) //"conversion from 'type1' to 'type2', possible loss of data"
 

OpenMPT/mptrack/Undo.cpp
@@ -14,6 +14,7 @@
 #include "MainFrm.h"
 #include "modsmp_ctrl.h"
 #include "Undo.h"
+#include "../common/StringFixer.h"
 
 #define new DEBUG_NEW
 

commit 219f8ac79d1576ef9d25b00a3bc99e254fcf8e88

[Fix] Do not overwrite non-empty song comments with zip file comments (broken since r1866 ).
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk@2928 56274372-70c3-4bfc-bfc3-4c3a0b034d27

OpenMPT/soundlib/Sndfile.cpp
@@ -742,7 +742,7 @@ BOOL CSoundFile::Create(FileReader file, ModLoadingFlags loadFlags)
 
 #ifndef NO_ARCHIVE_SUPPORT
 		// Read archive comment if there is no song comment
-		if(!songMessage.empty() && unarchiver.GetComments(false))
+		if(songMessage.empty() && unarchiver.GetComments(false))
 		{
 			songMessage.assign(unarchiver.GetComments(true));
 		}
devinacker/openmpt

commit 708e5e3c55d1ab41e2d0fa170b99b3f49c9a7cd8

[Ref] Remove the IFileDataContainer::GetPartialRawData dependency in FileReader::ReadMagic. IFileDataContainer::GetPartialRawData can result in precaching the whole file for some iFileDataContainer backends. [Ref] Remove the IFileDataContainer::GetPartialRawData dependency in FileReader::ReadString at the expence of a memory allocation for a temporary buffer. [Ref] Kill IFileDataContainer::GetPartialRawData.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk@4882 56274372-70c3-4bfc-bfc3-4c3a0b034d27

OpenMPT/common/FileReader.h
@@ -624,7 +624,9 @@ public:
 	{
 		if(CanRead(srcSize))
 		{
-			mpt::String::Read<mode, destSize>(destBuffer, DataContainer().GetPartialRawData(streamPos, srcSize), srcSize);
+			std::vector<char> tmp(srcSize);
+			DataContainer().Read(&tmp[0], streamPos, srcSize);
+			mpt::String::Read<mode, destSize>(destBuffer, &tmp[0], srcSize);
 			streamPos += srcSize;
 			return true;
 		} else
@@ -640,7 +642,9 @@ public:
 	{
 		if(CanRead(srcSize))
 		{
-			mpt::String::Read<mode>(dest, DataContainer().GetPartialRawData(streamPos, srcSize), srcSize);
+			std::vector<char> tmp(srcSize);
+			DataContainer().Read(&tmp[0], streamPos, srcSize);
+			mpt::String::Read<mode>(dest, &tmp[0], srcSize);
 			streamPos += srcSize;
 			return true;
 		} else
@@ -750,10 +754,21 @@ public:
 	// Returns false if the string could not be found. The file cursor is not advanced in this case.
 	bool ReadMagic(const char *const magic)
 	{
-		const off_t magicLength = strlen(magic);
+		const off_t magicLength = std::strlen(magic);
 		if(CanRead(magicLength))
 		{
-			if(!memcmp(DataContainer().GetPartialRawData(streamPos, magicLength), magic, magicLength))
+			bool identical = true;
+			for(std::size_t i = 0; i < magicLength; ++i)
+			{
+				char c = '\0';
+				DataContainer().Read(&c, streamPos + i, 1);
+				if(c != magic[i])
+				{
+					identical = false;
+					break;
+				}
+			}
+			if(identical)
 			{
 				streamPos += magicLength;
 				return true;

OpenMPT/common/mptIO.cpp
@@ -329,16 +329,6 @@ IFileDataContainer::off_t FileDataContainerStdStream::Read(char *dst, IFileDataC
 	return cache_avail;
 }
 
-const char *FileDataContainerStdStream::GetPartialRawData(IFileDataContainer::off_t pos, IFileDataContainer::off_t length) const
-{
-	CacheStreamUpTo(pos + length);
-	if(pos + length > IFileDataContainer::off_t(cachesize))
-	{
-		return nullptr;
-	}
-	return &cache[pos];
-}
-
 bool FileDataContainerStdStream::CanRead(IFileDataContainer::off_t pos, IFileDataContainer::off_t length) const
 {
 	CacheStreamUpTo(pos + length);

OpenMPT/common/mptIO.h
@@ -433,15 +433,6 @@ public:
 	virtual off_t GetLength() const = 0;
 	virtual off_t Read(char *dst, off_t pos, off_t count) const = 0;
 
-	virtual const char *GetPartialRawData(off_t pos, off_t length) const // DO NOT USE!!! this is just for ReadMagic ... the pointer returned may be invalid after the next Read()
-	{
-		if(pos + length > GetLength())
-		{
-			return nullptr;
-		}
-		return GetRawData() + pos;
-	}
-
 	virtual bool CanRead(off_t pos, off_t length) const
 	{
 		return pos + length <= GetLength();
@@ -511,14 +502,6 @@ public:
 			return 0;
 		}
 		return data->Read(dst, dataOffset + pos, std::min(count, dataLength - pos));
-	}
-	const char *GetPartialRawData(off_t pos, off_t length) const
-	{
-		if(pos + length > dataLength)
-		{
-			return nullptr;
-		}
-		return data->GetPartialRawData(dataOffset + pos, length);
 	}
 	bool CanRead(off_t pos, off_t length) const {
 		return (pos + length <= dataLength);
@@ -602,7 +585,6 @@ public:
 	const char *GetRawData() const;
 	off_t GetLength() const;
 	off_t Read(char *dst, off_t pos, off_t count) const;
-	const char *GetPartialRawData(off_t pos, off_t length) const;
 	bool CanRead(off_t pos, off_t length) const;
 	off_t GetReadableLength(off_t pos, off_t length) const;
 
@@ -661,15 +643,6 @@ public:
 		off_t avail = std::min<off_t>(streamLength - pos, count);
 		std::copy(streamData + pos, streamData + pos + avail, dst);
 		return avail;
-	}
-
-	const char *GetPartialRawData(off_t pos, off_t length) const
-	{
-		if(pos + length > streamLength)
-		{
-			return nullptr;
-		}
-		return streamData + pos;
 	}
 
 	bool CanRead(off_t pos, off_t length) const

commit 5639e9ea917aacb8637dbe4d4ad7bf2ab9fcf306

[Fix] FileReader: Fix buffer overrun in ReadString. (broken since r4882)
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk@4889 56274372-70c3-4bfc-bfc3-4c3a0b034d27

OpenMPT/common/FileReader.h
@@ -624,7 +624,7 @@ public:
 	{
 		if(CanRead(srcSize))
 		{
-			std::vector<char> tmp(srcSize);
+			std::vector<char> tmp(srcSize + 1); // +1 because tmp[0] does not work with srcSize==0 otherwise
 			DataContainer().Read(&tmp[0], streamPos, srcSize);
 			mpt::String::Read<mode, destSize>(destBuffer, &tmp[0], srcSize);
 			streamPos += srcSize;
@@ -642,7 +642,7 @@ public:
 	{
 		if(CanRead(srcSize))
 		{
-			std::vector<char> tmp(srcSize);
+			std::vector<char> tmp(srcSize + 1); // +1 because tmp[0] does not work with srcSize==0 otherwise
 			DataContainer().Read(&tmp[0], streamPos, srcSize);
 			mpt::String::Read<mode>(dest, &tmp[0], srcSize);
 			streamPos += srcSize;
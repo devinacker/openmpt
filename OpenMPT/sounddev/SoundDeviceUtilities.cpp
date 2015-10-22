devinacker/openmpt

commit 4d1b92b973f342ded9a62a37eccff3a604e92ea3

[Ref] sounddev: Convert avrt.dll handling to class Component.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk@5655 56274372-70c3-4bfc-bfc3-4c3a0b034d27

OpenMPT/sounddev/SoundDeviceUtilities.cpp
@@ -116,68 +116,73 @@ CAudioThread::~CAudioThread()
 }
 
 
+MPT_REGISTERED_COMPONENT(ComponentAvRt, "AvRt")
+
+ComponentAvRt::ComponentAvRt()
+//----------------------------
+	: ComponentLibrary(ComponentTypeSystem)
+	, AvSetMmThreadCharacteristicsW(nullptr)
+	, AvRevertMmThreadCharacteristics(nullptr)
+{
+	return;
+}
+
+bool ComponentAvRt::DoInitialize()
+//--------------------------------
+{
+	if(!mpt::Windows::Version::IsAtLeast(mpt::Windows::Version::WinVista))
+	{
+		return false;
+	}
+	AddLibrary("avrt", mpt::LibraryPath::System(MPT_PATHSTRING("avrt")));
+	MPT_COMPONENT_BIND("avrt", AvSetMmThreadCharacteristicsW);
+	MPT_COMPONENT_BIND("avrt", AvRevertMmThreadCharacteristics);
+	if(HasBindFailed())
+	{
+		return false;
+	}
+	return true;
+}
+
+ComponentAvRt::~ComponentAvRt()
+//-----------------------------
+{
+	return;
+}
+
+
 CPriorityBooster::CPriorityBooster(bool boostPriority)
 //----------------------------------------------------
-	: m_HasVista(false)
-	, pAvSetMmThreadCharacteristics(nullptr)
-	, pAvRevertMmThreadCharacteristics(nullptr)
-	, m_BoostPriority(boostPriority)
+	: m_BoostPriority(boostPriority)
 	, task_idx(0)
 	, hTask(NULL)
 {
-
 	MPT_TRACE();
-
-	m_HasVista = mpt::Windows::Version::IsAtLeast(mpt::Windows::Version::WinVista);
-
-	if(m_HasVista)
-	{
-		m_AvRtDLL = mpt::Library(mpt::LibraryPath::System(MPT_PATHSTRING("avrt")));
-		if(m_AvRtDLL.IsValid())
-		{
-			if(!m_AvRtDLL.Bind(pAvSetMmThreadCharacteristics, "AvSetMmThreadCharacteristicsA"))
-			{
-				m_HasVista = false;
-			}
-			if(!m_AvRtDLL.Bind(pAvRevertMmThreadCharacteristics, "AvRevertMmThreadCharacteristics"))
-			{
-				m_HasVista = false;
-			}
-		} else
-		{
-			m_HasVista = false;
-		}
-	}
-
 	#ifdef _DEBUG
 		m_BoostPriority = false;
 	#endif
-
 	if(m_BoostPriority)
 	{
-		if(m_HasVista)
+		if(IsComponentAvailable(m_AvRt))
 		{
-			hTask = pAvSetMmThreadCharacteristics(TEXT("Pro Audio"), &task_idx);
+			hTask = m_AvRt->AvSetMmThreadCharacteristicsW(L"Pro Audio", &task_idx);
 		} else
 		{
 			SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_ABOVE_NORMAL);
 		}
 	}
-
 }
 
 
 CPriorityBooster::~CPriorityBooster()
 //-----------------------------------
 {
-
 	MPT_TRACE();
-
 	if(m_BoostPriority)
 	{
-		if(m_HasVista)
+		if(IsComponentAvailable(m_AvRt))
 		{
-			pAvRevertMmThreadCharacteristics(hTask);
+			m_AvRt->AvRevertMmThreadCharacteristics(hTask);
 			hTask = NULL;
 			task_idx = 0;
 		} else
@@ -185,10 +190,6 @@ CPriorityBooster::~CPriorityBooster()
 			SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_NORMAL);
 		}
 	}
-
-	pAvRevertMmThreadCharacteristics = nullptr;
-	pAvSetMmThreadCharacteristics = nullptr;
-
 }
 
 

OpenMPT/sounddev/SoundDeviceUtilities.h
@@ -14,6 +14,7 @@
 #include "SoundDeviceBase.h"
 
 #include "../common/misc_util.h"
+#include "../common/ComponentManager.h"
 
 #if MPT_OS_WINDOWS
 #include <mmreg.h>
@@ -36,16 +37,25 @@ bool FillWaveFormatExtensible(WAVEFORMATEXTENSIBLE &WaveFormat, const SoundDevic
 class CSoundDeviceWithThread;
 
 
+class ComponentAvRt : public ComponentLibrary
+{
+	MPT_DECLARE_COMPONENT_MEMBERS
+public:
+	typedef HANDLE (WINAPI *pAvSetMmThreadCharacteristicsW)(LPCWSTR, LPDWORD);
+	typedef BOOL (WINAPI *pAvRevertMmThreadCharacteristics)(HANDLE);
+	pAvSetMmThreadCharacteristicsW AvSetMmThreadCharacteristicsW;
+	pAvRevertMmThreadCharacteristics AvRevertMmThreadCharacteristics;
+public:
+	ComponentAvRt();
+	virtual ~ComponentAvRt();
+	virtual bool DoInitialize();
+};
+
+
 class CPriorityBooster
 {
 private:
-	typedef HANDLE (WINAPI *FAvSetMmThreadCharacteristics)(LPCTSTR, LPDWORD);
-	typedef BOOL (WINAPI *FAvRevertMmThreadCharacteristics)(HANDLE);
-private:
-	bool m_HasVista;
-	mpt::Library m_AvRtDLL;
-	FAvSetMmThreadCharacteristics pAvSetMmThreadCharacteristics;
-	FAvRevertMmThreadCharacteristics pAvRevertMmThreadCharacteristics;
+	ComponentHandle<ComponentAvRt> m_AvRt;
 	bool m_BoostPriority;
 	DWORD task_idx;
 	HANDLE hTask;

commit 2de9d853b67707de8b14c9dbdc611e2bc8dec380

[Imp] sounddev: Reference ComponentAvRt from inside CAudioThread itself instead of only from within CPriorityBooster. This really moves loading and unloading to a more sensible place than potentially on every start or stop click. [Fix] sounddev: This also solves assertion in Settings.cpp caused by querying component blocking settings from within the audio thread when [Components]LoadOnStartup=0 (broken since r5655).
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk@5688 56274372-70c3-4bfc-bfc3-4c3a0b034d27

OpenMPT/sounddev/SoundDeviceUtilities.cpp
@@ -151,9 +151,10 @@ ComponentAvRt::~ComponentAvRt()
 }
 
 
-CPriorityBooster::CPriorityBooster(bool boostPriority)
-//----------------------------------------------------
-	: m_BoostPriority(boostPriority)
+CPriorityBooster::CPriorityBooster(ComponentHandle<ComponentAvRt> & avrt, bool boostPriority)
+//-------------------------------------------------------------------------------------------
+	: m_AvRt(avrt)
+	, m_BoostPriority(boostPriority)
 	, task_idx(0)
 	, hTask(NULL)
 {
@@ -337,7 +338,7 @@ DWORD CAudioThread::AudioThread()
 		if(!terminate)
 		{
 
-			CPriorityBooster priorityBooster(m_SoundDevice.m_Settings.BoostThreadPriority);
+			CPriorityBooster priorityBooster(m_AvRt, m_SoundDevice.m_Settings.BoostThreadPriority);
 			CPeriodicWaker periodicWaker(*this, m_WakeupInterval);
 
 			m_SoundDevice.StartFromSoundThread();

OpenMPT/sounddev/SoundDeviceUtilities.h
@@ -55,12 +55,12 @@ public:
 class CPriorityBooster
 {
 private:
-	ComponentHandle<ComponentAvRt> m_AvRt;
+	ComponentHandle<ComponentAvRt> & m_AvRt;
 	bool m_BoostPriority;
 	DWORD task_idx;
 	HANDLE hTask;
 public:
-	CPriorityBooster(bool boostPriority);
+	CPriorityBooster(ComponentHandle<ComponentAvRt> & avrt, bool boostPriority);
 	~CPriorityBooster();
 };
 
@@ -71,6 +71,7 @@ class CAudioThread
 private:
 	CSoundDeviceWithThread & m_SoundDevice;
 
+	ComponentHandle<ComponentAvRt> m_AvRt;
 	double m_WakeupInterval;
 	HANDLE m_hAudioWakeUp;
 	HANDLE m_hPlayThread;
devinacker/openmpt

commit 1da61d8ac7bb5624e7eca57d033226da268609cb

[Doc] Add some documentation to MPEGFrame::IsMPEGHeader [Fix] Protect against out-of-memory situations in mod unpackers [Mod] OpenMPT: Version is now 1.25.04.02
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk@5805 56274372-70c3-4bfc-bfc3-4c3a0b034d27

OpenMPT/soundlib/Mmcmp.cpp
@@ -204,7 +204,13 @@ bool UnpackMMCMP(std::vector<char> &unpackedData, FileReader &file)
 	if(mmh.blktable > file.GetLength()) return false;
 	if(mmh.blktable + 4 * mmh.nblocks > file.GetLength()) return false;
 
-	unpackedData.resize(mmh.filesize);
+	try
+	{
+		unpackedData.resize(mmh.filesize);
+	} catch(MPTMemoryException)
+	{
+		return false;
+	}
 
 	for (uint32 nBlock=0; nBlock<mmh.nblocks; nBlock++)
 	{
@@ -720,12 +726,12 @@ bool UnpackXPK(std::vector<char> &unpackedData, FileReader &file)
 #ifdef MMCMP_LOG
 	Log("XPK detected (SrcLen=%d DstLen=%d) filesize=%d\n", header.SrcLen, header.DstLen, file.GetLength());
 #endif
-	unpackedData.resize(header.DstLen);
 	bool result = false;
 	try
 	{
+		unpackedData.resize(header.DstLen);
 		result = XPK_DoUnpack(reinterpret_cast<const uint8 *>(file.GetRawData()), header.SrcLen + 8 - sizeof(XPKFILEHEADER), reinterpret_cast<uint8 *>(&(unpackedData[0])), header.DstLen);
-	} catch(XPK_error&)
+	} catch(...)
 	{
 		return false;
 	}
@@ -848,7 +854,13 @@ bool UnpackPP20(std::vector<char> &unpackedData, FileReader &file)
 	dstLen |= file.ReadUint8() << 8;
 	dstLen |= file.ReadUint8() << 0;
 	if(dstLen == 0) return false;
-	unpackedData.resize(dstLen);
+	try
+	{
+		unpackedData.resize(dstLen);
+	} catch(MPTMemoryException)
+	{
+		return false;
+	}
 	file.Seek(4);
 	bool result = PP20_DoUnpack(reinterpret_cast<const uint8 *>(file.GetRawData()), static_cast<uint32>(file.GetLength() - 4), reinterpret_cast<uint8 *>(&(unpackedData[0])), dstLen);
 

OpenMPT/common/versionNumber.h
@@ -19,7 +19,7 @@ OPENMPT_NAMESPACE_BEGIN
 #define VER_MAJORMAJOR          1
 #define VER_MAJOR               25
 #define VER_MINOR               04
-#define VER_MINORMINOR          01
+#define VER_MINORMINOR          02
 
 //Version string. For example "1.17.02.28"
 #define MPT_VERSION_STR         VER_STRINGIZE(VER_MAJORMAJOR) "." VER_STRINGIZE(VER_MAJOR) "." VER_STRINGIZE(VER_MINOR) "." VER_STRINGIZE(VER_MINORMINOR)

OpenMPT/soundlib/MPEGFrame.cpp
@@ -25,15 +25,15 @@ static const uint16 bitRates[2][3][15] =
 {
 	// MPEG 1
 	{
-		{ 0, 32, 64, 96, 128, 160, 192, 224, 256, 288, 320, 352, 384, 416, 448 },	// Layer1
-		{ 0, 32, 48, 56,  64,  80,  96, 112, 128, 160, 192, 224, 256, 320, 384 },	// Layer2
-		{ 0, 32, 40, 48,  56,  64,  80,  96, 112, 128, 160, 192, 224, 256, 320 }	// Layer3
+		{ 0, 32, 64, 96, 128, 160, 192, 224, 256, 288, 320, 352, 384, 416, 448 },	// Layer 1
+		{ 0, 32, 48, 56,  64,  80,  96, 112, 128, 160, 192, 224, 256, 320, 384 },	// Layer 2
+		{ 0, 32, 40, 48,  56,  64,  80,  96, 112, 128, 160, 192, 224, 256, 320 }	// Layer 3
 	},
 	// MPEG 2 / 2.5
 	{
-		{ 0, 32, 48, 56, 64, 80, 96, 112, 128, 144, 160, 176, 192, 224, 256 },	// Layer1
-		{ 0,  8, 16, 24, 32, 40, 48,  56,  64,  80,  96, 112, 128, 144, 160 },	// Layer2
-		{ 0,  8, 16, 24, 32, 40, 48,  56,  64,  80,  96, 112, 128, 144, 160 }	// Layer3
+		{ 0, 32, 48, 56, 64, 80, 96, 112, 128, 144, 160, 176, 192, 224, 256 },	// Layer 1
+		{ 0,  8, 16, 24, 32, 40, 48,  56,  64,  80,  96, 112, 128, 144, 160 },	// Layer 2
+		{ 0,  8, 16, 24, 32, 40, 48,  56,  64,  80,  96, 112, 128, 144, 160 }	// Layer 3
 	}
 };
 // Sampling rates for each MPEG version and all three layers
@@ -61,7 +61,11 @@ static const uint8 sideInfoSize[2][2] =
 bool MPEGFrame::IsMPEGHeader(const uint8 (&header)[3])
 //----------------------------------------------------
 {
-	return header[0] == 0xFF && (header[1] & 0xE0) == 0xE0 && (header[1] & 0x18) != 0x08 && (header[1] & 0x06) != 0x00 && (header[2] & 0x0C) != 0x0C && (header[2] & 0xF0) != 0xF0;
+	return header[0] == 0xFF && (header[1] & 0xE0) == 0xE0	// Sync
+		&& (header[1] & 0x18) != 0x08	// Invalid MPEG version
+		&& (header[1] & 0x06) != 0x00	// Invalid MPEG layer
+		&& (header[2] & 0x0C) != 0x0C	// Invalid frequency
+		&& (header[2] & 0xF0) != 0xF0;	// Invalid bitrate
 }
 
 
@@ -105,6 +109,7 @@ MPEGFrame::MPEGFrame(FileReader &file)
 
 	uint8 magic[4];
 	file.ReadArray(magic);
+	// This is all we really need to know for our purposes in the MO3 decoder.
 	isLAME = !memcmp(magic, "Info", 4) || !memcmp(magic, "Xing", 4);
 }
 

commit 1dc388e27d6e7cb651e2566d2bed09afc752d7bd

[Fix] XPK: Do not silently discard all memory corruption bugs in tracker builds. We cannot use catch(...) to catch multiple c++ excpetions because we are compiling the tracker with /EHa which causes SEH exceptions to masquerade as exceptions that catch can catch. Explicitely list the exceptions we are interested in here (XPK_error and MPTMemoryException). (broken since r5805)
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk@5840 56274372-70c3-4bfc-bfc3-4c3a0b034d27

OpenMPT/soundlib/Mmcmp.cpp
@@ -731,7 +731,10 @@ bool UnpackXPK(std::vector<char> &unpackedData, FileReader &file)
 	{
 		unpackedData.resize(header.DstLen);
 		result = XPK_DoUnpack(reinterpret_cast<const uint8 *>(file.GetRawData()), header.SrcLen + 8 - sizeof(XPKFILEHEADER), reinterpret_cast<uint8 *>(&(unpackedData[0])), header.DstLen);
-	} catch(...)
+	} catch(MPTMemoryException)
+	{
+		return false;
+	} catch(const XPK_error &)
 	{
 		return false;
 	}
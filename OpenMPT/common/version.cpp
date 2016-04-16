devinacker/openmpt

commit 2e4a0e758c16d5189b35e3c3b1e73fae570ccffd

[Ref] Move most openmpt.org link strings to MptVersion::GetURL(), and small related cleanups.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk@6257 56274372-70c3-4bfc-bfc3-4c3a0b034d27

OpenMPT/common/version.cpp
@@ -514,15 +514,33 @@ std::string SourceInfo::GetUrlWithRevision() const
 	return Url + "@" + mpt::ToString(Revision);
 }
 
-mpt::ustring GetContactString()
+mpt::ustring GetURL(std::string key)
 {
-	return MPT_UTF8(
-		"Contact / Discussion:\n"
-		"https://forum.openmpt.org/\n"
-		"\n"
-		"Updates:\n"
-		"https://openmpt.org/download\n"
-		);
+	mpt::ustring result;
+	if(key.empty())
+	{
+		result = mpt::ustring();
+	} else if(key == "website")
+	{
+		#ifdef LIBOPENMPT_BUILD
+			result = MPT_USTRING("https://lib.openmpt.org/");
+		#else
+			result = MPT_USTRING("https://openmpt.org/");
+		#endif
+	} else if(key == "forum")
+	{
+		result = MPT_USTRING("https://forum.openmpt.org/");
+	} else if(key == "bugtracker")
+	{
+		result = MPT_USTRING("https://bugs.openmpt.org/");
+	} else if(key == "updates")
+	{
+		result = MPT_USTRING("https://openmpt.org/download");
+	} else if(key == "top_picks")
+	{
+		result = MPT_USTRING("https://openmpt.org/top_pick");
+	}
+	return result;
 }
 
 mpt::ustring GetFullCreditsString()

OpenMPT/common/version.h
@@ -87,11 +87,11 @@ namespace MptVersion
 	// Returns MptVersion::str if the build is a clean release build straight from the repository or an extended string otherwise (if built from a svn working copy and tsvn was available during build)
 	std::string GetVersionStringExtended(); // e.g. "1.17.02.08-r1234+ 32 bit DEBUG"
 
+	// Returns a URL for the respective keys. Supported keys: "website", "forum", "bugtracker", "updates", "top_picks"
+	mpt::ustring GetURL(std::string key);
+
 	// Returns a multi-line string containing the full credits for the code base
 	mpt::ustring GetFullCreditsString();
-
-	// Returns a multi-line string containing developer contact and community addresses
-	mpt::ustring GetContactString();
 
 	// Returns the OpenMPT license text
 	mpt::ustring GetLicenseString();

OpenMPT/libopenmpt/libopenmpt.h
@@ -148,7 +148,10 @@ LIBOPENMPT_API void openmpt_free_string( const char * str );
  *        -  "build_compiler": information about the compiler used to build libopenmpt
  *        -  "credits": all contributors
  *        -  "contact": contact infromation about libopenmpt
- *        -  "license": he libopenmpt license
+ *        -  "license": the libopenmpt license
+ *        -  "url": libopenmpt website URL
+ *        -  "support_forum_url": libopenmpt support and discussions forum URL
+ *        -  "bugtracker_url": libopenmpt bug and issue tracker URL
  * \return A (possibly multi-line) string containing the queried information. If no information is available, the string is empty.
  */
 LIBOPENMPT_API const char * openmpt_get_string( const char * key );

OpenMPT/libopenmpt/libopenmpt.hpp
@@ -133,19 +133,23 @@ static const char license         [] = "license";
 
 //! Get library related metadata.
 /*!
-	\param key Key to query.
-	       Possible keys are:
-	        -  "library_version": verbose library version string
-	        -  "library_features": verbose library features string
-	        -  "core_version": verboseOpenMPT core version string
-	        -  "source_url": original source code URL
-	        -  "source_date": original source code date
-	        -  "build": information about the current build (e.g. the build date or compiler used)
-	        -  "build_compiler": information about the compiler used to build libopenmpt
-	        -  "credits": all contributors
-	        -  "contact": contact infromation about libopenmpt
-	        -  "license": he libopenmpt license
-	\return A (possibly multi-line) string containing the queried information. If no information is available, the string is empty.
+  \param key Key to query.
+         Possible keys are:
+          -  "library_version": verbose library version string
+          -  "library_features": verbose library features string
+          -  "core_version": verboseOpenMPT core version string
+          -  "source_url": original source code URL
+          -  "source_date": original source code date
+          -  "build": information about the current build (e.g. the build date or compiler used)
+          -  "build_compiler": information about the compiler used to build libopenmpt
+          -  "credits": all contributors
+          -  "contact": contact infromation about libopenmpt
+          -  "license": the libopenmpt license
+          -  "url": libopenmpt website URL
+          -  "support_forum_url": libopenmpt support and discussions forum URL
+          -  "bugtracker_url": libopenmpt bug and issue tracker URL
+
+  \return A (possibly multi-line) string containing the queried information. If no information is available, the string is empty.
 */
 LIBOPENMPT_CXX_API std::string get( const std::string & key );
 

OpenMPT/libopenmpt/libopenmpt_impl.cpp
@@ -150,11 +150,23 @@ static std::string get_credits_string() {
 }
 
 static std::string get_contact_string() {
-	return mpt::ToCharset(mpt::CharsetUTF8, MptVersion::GetContactString());
+	return mpt::ToCharset(mpt::CharsetUTF8, MPT_USTRING("Forum: ") +  MptVersion::GetURL("forum"));
 }
 
 static std::string get_license_string() {
 	return mpt::ToCharset(mpt::CharsetUTF8, MptVersion::GetLicenseString());
+}
+
+static std::string get_url_string() {
+	return mpt::ToCharset(mpt::CharsetUTF8, MptVersion::GetURL("website"));
+}
+
+static std::string get_support_forum_url_string() {
+	return mpt::ToCharset(mpt::CharsetUTF8, MptVersion::GetURL("forum"));
+}
+
+static std::string get_bugtracker_url_string() {
+	return mpt::ToCharset(mpt::CharsetUTF8, MptVersion::GetURL("bugtracker"));
 }
 
 std::string get_string( const std::string & key ) {
@@ -180,6 +192,12 @@ std::string get_string( const std::string & key ) {
 		return get_contact_string();
 	} else if ( key == "license" ) {
 		return get_license_string();
+	} else if ( key == "url" ) {
+		return get_url_string();
+	} else if ( key == "support_forum_url" ) {
+		return get_support_forum_url_string();
+	} else if ( key == "bugtracker_url" ) {
+		return get_bugtracker_url_string();
 	} else {
 		return std::string();
 	}

OpenMPT/mptrack/AboutDialog.cpp
@@ -266,7 +266,7 @@ BOOL CAboutDlg::OnInitDialog()
 		+ MPT_USTRING("\n");
 	app += MPT_USTRING("Version ") + mpt::ToUnicode(mpt::CharsetUTF8, MptVersion::GetVersionStringSimple()) + MPT_USTRING("\n");
 	app += MPT_USTRING("\n");
-	app += MPT_USTRING("https://openmpt.org/\n");
+	app += MptVersion::GetURL("website") + MPT_USTRING("\n");
 	SetDlgItemText(IDC_EDIT3, mpt::ToCString(mpt::String::Replace(app, MPT_USTRING("\n"), MPT_USTRING("\r\n"))));
 
 	m_bmp.SubclassDlgItem(IDC_BITMAP1, this);
@@ -275,7 +275,7 @@ BOOL CAboutDlg::OnInitDialog()
 	m_Tab.InsertItem(TCIF_TEXT, 1, _T("Components"), 0, 0, 0, 0);
 	m_Tab.InsertItem(TCIF_TEXT, 2, _T("Credits"), 0, 0, 0, 0);
 	m_Tab.InsertItem(TCIF_TEXT, 3, _T("License"), 0, 0, 0, 0);
-	m_Tab.InsertItem(TCIF_TEXT, 4, _T("Contact"), 0, 0, 0, 0);
+	m_Tab.InsertItem(TCIF_TEXT, 4, _T("Resources"), 0, 0, 0, 0);
 	m_Tab.SetCurSel(0);
 
 	OnTabChange(nullptr, nullptr);
@@ -479,7 +479,15 @@ mpt::ustring CAboutDlg::GetTabText(int tab)
 			text += MptVersion::GetLicenseString();
 			break;
 		case 4:
-			text += MptVersion::GetContactString();
+			text += lf;
+			text += MPT_USTRING("Website: ") + lf + MptVersion::GetURL("website") + lf;
+			text += lf;
+			text += MPT_USTRING("Forum: ") + lf + MptVersion::GetURL("forum") + lf;
+			text += lf;
+			text += MPT_USTRING("Bug Tracker: ") + lf + MptVersion::GetURL("bugtracker") + lf;
+			text += lf;
+			text += MPT_USTRING("Updates: ") + lf + MptVersion::GetURL("updates") + lf;
+			text += lf;
 			break;
 	}
 	return text;

OpenMPT/mptrack/MainFrm.cpp
@@ -2375,24 +2375,22 @@ void CMainFrame::OnOctaveChanged()
 void CMainFrame::OnReportBug()
 //----------------------------
 {
-	CTrackApp::OpenURL("https://bugs.openmpt.org/");
-	return;
+	CTrackApp::OpenURL(MptVersion::GetURL("bugtracker"));
 }
 
 
 BOOL CMainFrame::OnInternetLink(UINT nID)
 //---------------------------------------
 {
-	LPCSTR pszURL = nullptr;
-
+	mpt::ustring url;
 	switch(nID)
 	{
-	case ID_NETLINK_MODPLUG:	pszURL = "https://openmpt.org/"; break;
-	case ID_NETLINK_TOP_PICKS:	pszURL = "https://openmpt.org/top_picks"; break;
+	case ID_NETLINK_MODPLUG:	url = MptVersion::GetURL("website"); break;
+	case ID_NETLINK_TOP_PICKS:	url = MptVersion::GetURL("top_picks"); break;
 	}
-	if(pszURL != nullptr)
+	if(!url.empty())
 	{
-		return CTrackApp::OpenURL(pszURL) ? TRUE : FALSE;
+		return CTrackApp::OpenURL(url) ? TRUE : FALSE;
 	}
 	return FALSE;
 }

OpenMPT/openmpt123/openmpt123.cpp
@@ -364,7 +364,7 @@ static std::string seconds_to_string( double time ) {
 
 static void show_info( std::ostream & log, bool verbose ) {
 	log << "openmpt123" << " v" << OPENMPT123_VERSION_STRING << ", libopenmpt " << openmpt::string::get( "library_version" ) << " (" << "OpenMPT " << openmpt::string::get( "core_version" ) << ")" << std::endl;
-	log << "Copyright (c) 2013-2016 OpenMPT developers <https://openmpt.org/>" << std::endl;
+	log << "Copyright (c) 2013-2016 OpenMPT developers <https://lib.openmpt.org/>" << std::endl;
 	if ( !verbose ) {
 		log << std::endl;
 		return;
@@ -418,7 +418,7 @@ static void show_info( std::ostream & log, bool verbose ) {
 }
 
 static void show_man_version( textout & log ) {
-	log << "openmpt123" << " v" << OPENMPT123_VERSION_STRING << ", " << "Copyright (c) 2013-2016 OpenMPT developers <https://openmpt.org/>" << std::endl;
+	log << "openmpt123" << " v" << OPENMPT123_VERSION_STRING << ", " << "Copyright (c) 2013-2016 OpenMPT developers <https://lib.openmpt.org/>" << std::endl;
 }
 
 static void show_short_version( textout & log ) {

commit 5dde0fe5752c3657ba8eac012bd0814460df5240

[Fix] Top picks link got broken in r6257. [Doc] Update libopenmpt.bi documentation to mention the new strings introduced in r6257. [Doc] Update libopenmpt changelog.md.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk@6262 56274372-70c3-4bfc-bfc3-4c3a0b034d27

OpenMPT/common/version.cpp
@@ -538,7 +538,7 @@ mpt::ustring GetURL(std::string key)
 		result = MPT_USTRING("https://openmpt.org/download");
 	} else if(key == "top_picks")
 	{
-		result = MPT_USTRING("https://openmpt.org/top_pick");
+		result = MPT_USTRING("https://openmpt.org/top_picks");
 	}
 	return result;
 }

OpenMPT/libopenmpt/bindings/freebasic/libopenmpt.bi
@@ -70,7 +70,10 @@ Declare Sub openmpt_free_string(ByVal Str As Const ZString Ptr)
  '        -  "build_compiler": information about the compiler used to build libopenmpt
  '        -  "credits": all contributors
  '        -  "contact": contact infromation about libopenmpt
- '        -  "license": he libopenmpt license
+ '        -  "license": the libopenmpt license
+ '        -  "url": libopenmpt website URL
+ '        -  "support_forum_url": libopenmpt support and discussions forum URL
+ '        -  "bugtracker_url": libopenmpt bug and issue tracker URL
  ' \return A (possibly multi-line) string containing the queried information. If no information is available, the string is empty.
  ' \remarks Use openmpt_get_string to automatically handle the lifetime of the returned pointer.
  '/
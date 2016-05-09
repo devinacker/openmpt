devinacker/openmpt

commit f31dfff0141588c9769ad7af4ad67e4a57b9941b

[Mod] Pattern tab: Reset beat highlight together with measure higlight (http://bugs.openmpt.org/view.php?id=662) [Imp] Pattern tab: Smooth scrolling also stops scrolling now when staying on the same row.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk@5391 56274372-70c3-4bfc-bfc3-4c3a0b034d27

OpenMPT/mptrack/Draw_pat.cpp
@@ -163,11 +163,12 @@ int CViewPattern::GetSmoothScrollOffset() const
 	if((TrackerSettings::Instance().m_dwPatternSetup & PATTERN_SMOOTHSCROLL) != 0	// Actually using the smooth scroll feature
 		&& (m_Status & (psFollowSong | psDragActive)) == psFollowSong	// Not drawing a selection during playback
 		&& (m_nMidRow != 0 || GetYScrollPos() > 0)	// If active row is not centered, only scroll when display position is actually not at the top
-		&& IsLiveRecord())	// Actually playing live (not paused or stepping)
+		&& IsLiveRecord()	// Actually playing live (not paused or stepping)
+		&& m_nNextPlayRow != m_nPlayRow)	// Don't scroll if we stay on the same row
 	{
 		uint32 tick = m_nPlayTick;
 		// Avoid jerky animation with backwards-going patterns
-		if(m_smoothScrollBackwards) tick = m_nTicksOnRow - m_nPlayTick - 1;
+		if(m_nNextPlayRow == m_nPlayRow - 1) tick = m_nTicksOnRow - m_nPlayTick - 1;
 		return Util::muldivr_unsigned(m_szCell.cy, tick, std::max(1u, m_nTicksOnRow));
 	}
 	return 0;
@@ -822,19 +823,20 @@ void CViewPattern::DrawPatternData(HDC hdc, PATTERNINDEX nPattern, bool selEnabl
 			nMeasure = sndFile.Patterns[nPattern].GetRowsPerMeasure();
 		}
 		// secondary highlight (beats)
+		ROWINDEX highlightRow = compRow % nMeasure;
 		if ((TrackerSettings::Instance().m_dwPatternSetup & PATTERN_2NDHIGHLIGHT)
-			&& (nBeat) && (nBeat < numRows))
+			&& nBeat > 0)
 		{
-			if(!(compRow % nBeat))
+			if((highlightRow % nBeat) == 0)
 			{
 				row_bkcol = MODCOLOR_2NDHIGHLIGHT;
 			}
 		}
 		// primary highlight (measures)
 		if((TrackerSettings::Instance().m_dwPatternSetup & PATTERN_STDHIGHLIGHT)
-			&& (nMeasure) && (nMeasure < numRows))
+			&& nMeasure > 0)
 		{
-			if(!(compRow % nMeasure))
+			if(highlightRow == 0)
 			{
 				row_bkcol = MODCOLOR_BACKHILIGHT;
 			}

OpenMPT/mptrack/View_pat.cpp
@@ -184,14 +184,13 @@ void CViewPattern::OnInitialUpdate()
 	memset(splitActiveNoteChannel, 0xFF, sizeof(splitActiveNoteChannel));
 	memset(activeNoteChannel, 0xFF, sizeof(activeNoteChannel));
 	m_nPlayPat = PATTERNINDEX_INVALID;
-	m_nPlayRow = 0;
+	m_nPlayRow = m_nNextPlayRow = 0;
 	m_nPlayTick = 0;
 	m_nTicksOnRow = 1;
 	m_nMidRow = 0;
 	m_nDragItem = 0;
 	m_bInItemRect = false;
 	m_bContinueSearch = false;
-	m_smoothScrollBackwards = false;
 	m_Status = psShowPluginNames;
 	m_nXScroll = m_nYScroll = 0;
 	m_nPattern = 0;
@@ -3764,12 +3763,13 @@ LRESULT CViewPattern::OnPlayerNotify(Notification *pnotify)
 			}
 
 			// Simple detection of backwards-going patterns to avoid jerky animation
-			m_smoothScrollBackwards = false;
+			m_nNextPlayRow = nRow + 1;
 			if((TrackerSettings::Instance().m_dwPatternSetup & PATTERN_SMOOTHSCROLL) && pSndFile->Patterns.IsValidPat(nPat))
 			{
 				for(const ModCommand *m = pSndFile->Patterns[nPat].GetRow(nRow), *mEnd = m + pSndFile->GetNumChannels(); m != mEnd; m++)
 				{
-					if(m->command == CMD_PATTERNBREAK) m_smoothScrollBackwards = (m->param == nRow - 1);
+					if(m->command == CMD_PATTERNBREAK) m_nNextPlayRow = m->param;
+					else if(m->command == CMD_POSITIONJUMP && pSndFile->GetType() == MOD_TYPE_XM) m_nNextPlayRow = 0;
 				}
 			}
 

OpenMPT/mptrack/View_pat.h
@@ -165,7 +165,7 @@ protected:
 	CRect m_oldClient;
 	UINT m_nMidRow, m_nSpacing, m_nAccelChar, m_nLastPlayedRow, m_nLastPlayedOrder;
 	FlagSet<PatternStatus> m_Status;
-	ROWINDEX m_nPlayRow;
+	ROWINDEX m_nPlayRow, m_nNextPlayRow;
 	uint32 m_nPlayTick, m_nTicksOnRow;
 	PATTERNINDEX m_nPattern, m_nPlayPat;
 	ORDERINDEX m_nOrder;
@@ -190,7 +190,6 @@ protected:
 	DWORD m_dwLastNoteEntryTime; //rewbs.customkeys
 	bool m_bLastNoteEntryBlocked : 1;
 	bool m_bContinueSearch : 1, m_bWholePatternFitsOnScreen : 1;
-	bool m_smoothScrollBackwards : 1;
 
 	ModCommand m_PCNoteEditMemory;		// PC Note edit memory
 	static ModCommand m_cmdOld;			// Quick cursor copy/paste data

commit 171bc56155297f33ec10e0f481bcdaf3dcbd7783

[Fix] Pattern tab: Patterns were not drawn at all when rows per measure was set to 0 (broken since r5391).
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk@6341 56274372-70c3-4bfc-bfc3-4c3a0b034d27

OpenMPT/mptrack/Draw_pat.cpp
@@ -835,7 +835,9 @@ void CViewPattern::DrawPatternData(HDC hdc, PATTERNINDEX nPattern, bool selEnabl
 			nMeasure = sndFile.Patterns[nPattern].GetRowsPerMeasure();
 		}
 		// secondary highlight (beats)
-		ROWINDEX highlightRow = compRow % nMeasure;
+		ROWINDEX highlightRow = compRow;
+		if(nMeasure > 0)
+			highlightRow %= nMeasure;
 		if ((TrackerSettings::Instance().m_dwPatternSetup & PATTERN_2NDHIGHLIGHT)
 			&& nBeat > 0)
 		{
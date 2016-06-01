devinacker/openmpt

commit 9def89005c0a956d3a31f3f4a98d3eef60ca18ee

[Imp] Allow fractional inputs for CInputDlg.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk@5939 56274372-70c3-4bfc-bfc3-4c3a0b034d27

OpenMPT/mptrack/dlg_misc.cpp
@@ -1406,36 +1406,47 @@ BOOL CInputDlg::OnInitDialog()
 	ScreenToClient(cancelRect);
 
 	// Find out how big our label shall be
-	CSize size;
 	HDC dc = ::GetDC(m_hWnd);
-	GetTextExtentPoint32(dc, description, strlen(description), &size);
+	CRect textRect;
+	DrawText(dc, description, description.GetLength(), textRect, DT_CALCRECT);
+	LPtoDP(dc, &textRect.BottomRight(), 1);
 	::ReleaseDC(m_hWnd, dc);
-	if(size.cx < 320) size.cx = 320;
-	const int windowWidth = windowRect.Width() - labelRect.Width() + size.cx;
-	const int windowHeight = windowRect.Height() - labelRect.Height() + size.cy;
+	if(textRect.right < 320) textRect.right = 320;
+	const int windowWidth = windowRect.Width() - labelRect.Width() + textRect.right;
+	const int windowHeight = windowRect.Height() - labelRect.Height() + textRect.bottom;
 
 	// Resize and move all controls
-	GetDlgItem(IDC_PROMPT)->SetWindowPos(nullptr, 0, 0, size.cx, size.cy, SWP_NOMOVE | SWP_NOZORDER);
-	GetDlgItem(IDC_EDIT1)->SetWindowPos(nullptr, inputRect.left, labelRect.top + size.cy + (inputRect.top - labelRect.bottom), size.cx, inputRect.Height(), SWP_NOZORDER);
+	GetDlgItem(IDC_PROMPT)->SetWindowPos(nullptr, 0, 0, textRect.right, textRect.bottom, SWP_NOMOVE | SWP_NOZORDER);
+	GetDlgItem(IDC_EDIT1)->SetWindowPos(nullptr, inputRect.left, labelRect.top + textRect.bottom + (inputRect.top - labelRect.bottom), textRect.right, inputRect.Height(), SWP_NOZORDER);
 	GetDlgItem(IDOK)->SetWindowPos(nullptr, windowWidth - (windowRect.Width() - okRect.left), windowHeight - (windowRect.Height() - okRect.top), 0, 0, SWP_NOSIZE | SWP_NOZORDER);
 	GetDlgItem(IDCANCEL)->SetWindowPos(nullptr, windowWidth - (windowRect.Width() - cancelRect.left), windowHeight - (windowRect.Height() - cancelRect.top), 0, 0, SWP_NOSIZE | SWP_NOZORDER);
 	SetWindowPos(nullptr, 0, 0, windowWidth, windowHeight, SWP_NOMOVE | SWP_NOZORDER);
 
-	if(minValue != maxValue)
+	if(m_minValueInt != m_maxValueInt)
 	{
-		// Numeric
-		spin.SetRange32(minValue, maxValue);
+		// Numeric (int)
+		spin.SetRange32(m_minValueInt, m_maxValueInt);
 		spin.SetBuddy(GetDlgItem(IDC_EDIT1));
-		SetDlgItemInt(IDC_EDIT1, resultNumber);
+		SetDlgItemInt(IDC_EDIT1, resultAsInt);
 		m_edit.SubclassDlgItem(IDC_EDIT1, this);
 		m_edit.ModifyStyle(0, ES_NUMBER);
-		m_edit.AllowNegative(minValue < 0);
+		m_edit.AllowNegative(m_minValueInt < 0);
 		m_edit.AllowFractions(false);
+	} else if(m_minValueDbl != m_maxValueDbl)
+	{
+		// Numeric (double)
+		spin.SetRange32(static_cast<int32>(m_minValueDbl), static_cast<int32>(m_maxValueDbl));
+		spin.SetBuddy(GetDlgItem(IDC_EDIT1));
+		m_edit.SubclassDlgItem(IDC_EDIT1, this);
+		m_edit.ModifyStyle(0, ES_NUMBER);
+		m_edit.AllowNegative(m_minValueDbl < 0);
+		m_edit.AllowFractions(true);
+		m_edit.SetDecimalValue(resultAsDouble);
 	} else
 	{
 		// Text
 		spin.ShowWindow(SW_HIDE);
-		SetDlgItemText(IDC_EDIT1, resultString);
+		SetDlgItemText(IDC_EDIT1, resultAsString);
 	}
 
 	return TRUE;
@@ -1446,8 +1457,9 @@ void CInputDlg::OnOK()
 //--------------------
 {
 	CDialog::OnOK();
-	GetDlgItemText(IDC_EDIT1, resultString);
-	resultNumber = static_cast<int32>(GetDlgItemInt(IDC_EDIT1));
+	GetDlgItemText(IDC_EDIT1, resultAsString);
+	resultAsInt = static_cast<int32>(GetDlgItemInt(IDC_EDIT1));
+	m_edit.GetDecimalValue(resultAsDouble);
 }
 
 

OpenMPT/mptrack/AbstractVstEditor.cpp
@@ -272,11 +272,11 @@ void CAbstractVstEditor::OnPasteParameters()
 void CAbstractVstEditor::OnRandomizePreset()
 //-----------------------------------------
 {
-	static int randomFactor = 10;
-	CInputDlg dlg(this, _T("Input parameter randomization amount (0 = no change, 100 = completely random)"), 0, 100, randomFactor);
+	static double randomFactor = 10.0;
+	CInputDlg dlg(this, _T("Input parameter randomization amount (0 = no change, 100 = completely random)"), 0.0, 100.0, randomFactor);
 	if(dlg.DoModal() == IDOK)
 	{
-		randomFactor = dlg.resultNumber;
+		randomFactor = dlg.resultAsDouble;
 		PlugParamValue factor = PlugParamValue(randomFactor) / 100.0f;
 		PlugParamIndex numParams = m_VstPlugin.GetNumParameters();
 		for(PlugParamIndex p = 0; p < numParams; p++)

OpenMPT/mptrack/AdvancedConfigDlg.cpp
@@ -289,7 +289,7 @@ void COptionsAdvanced::OnOptionDblClick(NMHDR *, LRESULT *)
 		{
 			return;
 		}
-		val.SetFromString(inputDlg.resultString);
+		val.SetFromString(inputDlg.resultAsString);
 	}
 	theApp.GetSettings().Write(path, val);
 	m_List.SetItemText(index, 2, val.FormatValueAsString().c_str());

OpenMPT/mptrack/View_pat.cpp
@@ -2905,10 +2905,10 @@ void CViewPattern::Interpolate(PatternCursor::Columns type)
 void CViewPattern::OnTransposeChannel()
 //-------------------------------------
 {
-	CInputDlg dlg(this, "Enter transpose amount (affects all patterns):", -(NOTE_MAX - NOTE_MIN), (NOTE_MAX - NOTE_MIN), m_nTransposeAmount);
+	CInputDlg dlg(this, _T("Enter transpose amount (affects all patterns):"), -(NOTE_MAX - NOTE_MIN), (NOTE_MAX - NOTE_MIN), m_nTransposeAmount);
 	if(dlg.DoModal() == IDOK)
 	{
-		m_nTransposeAmount = dlg.resultNumber;
+		m_nTransposeAmount = dlg.resultAsInt;
 
 		CSoundFile &sndFile = *GetSoundFile();
 		bool changed = false;
@@ -2952,11 +2952,11 @@ void CViewPattern::OnTransposeChannel()
 void CViewPattern::OnTransposeCustom()
 //------------------------------------
 {
-	CInputDlg dlg(this, "Enter transpose amount:", -(NOTE_MAX - NOTE_MIN), (NOTE_MAX - NOTE_MIN), m_nTransposeAmount);
+	CInputDlg dlg(this, _T("Enter transpose amount:"), -(NOTE_MAX - NOTE_MIN), (NOTE_MAX - NOTE_MIN), m_nTransposeAmount);
 	if(dlg.DoModal() == IDOK)
 	{
-		m_nTransposeAmount = dlg.resultNumber;
-		TransposeSelection(dlg.resultNumber);
+		m_nTransposeAmount = dlg.resultAsInt;
+		TransposeSelection(dlg.resultAsInt);
 	}
 }
 
@@ -5967,10 +5967,10 @@ PATTERNINDEX CViewPattern::GetNextPattern() const
 void CViewPattern::OnSetQuantize()
 //--------------------------------
 {
-	CInputDlg dlg(this, "Quantize amount in rows for live recording (0 to disable):", 0, MAX_PATTERN_ROWS, TrackerSettings::Instance().recordQuantizeRows);
+	CInputDlg dlg(this, _T("Quantize amount in rows for live recording (0 to disable):"), 0, MAX_PATTERN_ROWS, TrackerSettings::Instance().recordQuantizeRows);
 	if(dlg.DoModal())
 	{
-		TrackerSettings::Instance().recordQuantizeRows = static_cast<ROWINDEX>(dlg.resultNumber);
+		TrackerSettings::Instance().recordQuantizeRows = static_cast<ROWINDEX>(dlg.resultAsInt);
 	}
 }
 

OpenMPT/mptrack/dlg_misc.h
@@ -279,19 +279,34 @@ protected:
 	CNumberEdit m_edit;
 	CSpinButtonCtrl spin;
 	CString description;
-	int32 minValue, maxValue;
+	double m_minValueDbl, m_maxValueDbl;
+	int32 m_minValueInt, m_maxValueInt;
 
 public:
-	CString resultString;
-	int32 resultNumber;
+	CString resultAsString;
+	double resultAsDouble;
+	int32 resultAsInt;
 
 public:
 	// Initialize text input box
-	CInputDlg(CWnd *parent, const char *desc, const char *defaultString) : CDialog(IDD_INPUT, parent),
-		description(desc), minValue(0), maxValue(0), resultString(defaultString) { }
-	// Initialize numeric input box
-	CInputDlg(CWnd *parent, const char *desc, int32 minVal, int32 maxVal, int32 defaultNumber) : CDialog(IDD_INPUT, parent),
-		description(desc), minValue(minVal), maxValue(maxVal), resultNumber(defaultNumber) { }
+	CInputDlg(CWnd *parent, const TCHAR *desc, const char *defaultString) : CDialog(IDD_INPUT, parent)
+		, description(desc)
+		, resultAsString(defaultString)
+		, m_minValueDbl(0), m_maxValueDbl(0), resultAsDouble(0)
+		, m_minValueInt(0), m_maxValueInt(0), resultAsInt(0)
+	{ }
+	// Initialize numeric input box (float)
+	CInputDlg(CWnd *parent, const TCHAR *desc, double minVal, double maxVal, double defaultNumber) : CDialog(IDD_INPUT, parent)
+		, description(desc)
+		, m_minValueDbl(minVal), m_maxValueDbl(maxVal), resultAsDouble(defaultNumber)
+		, m_minValueInt(0), m_maxValueInt(0), resultAsInt(0)
+	{ }
+	// Initialize numeric input box (int)
+	CInputDlg(CWnd *parent, const TCHAR *desc, int32 minVal, int32 maxVal, int32 defaultNumber) : CDialog(IDD_INPUT, parent)
+		, description(desc)
+		, m_minValueDbl(0), m_maxValueDbl(0), resultAsDouble(0)
+		, m_minValueInt(minVal), m_maxValueInt(maxVal), resultAsInt(defaultNumber)
+	{ }
 
 protected:
 	virtual void DoDataExchange(CDataExchange* pDX);

commit 276de843579c514cf6420b0ea6e20a53eaf415aa

[Fix] DrawText(DT_CALCRECT) is specified to expand the passed RECT and not to set it. Fixes <https://bugs.openmpt.org/view.php?id=810>. Broken since r5939.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk@6460 56274372-70c3-4bfc-bfc3-4c3a0b034d27

OpenMPT/mptrack/dlg_misc.cpp
@@ -1416,7 +1416,7 @@ BOOL CInputDlg::OnInitDialog()
 
 	// Find out how big our label shall be
 	HDC dc = ::GetDC(m_hWnd);
-	CRect textRect;
+	CRect textRect(0,0,0,0);
 	DrawText(dc, m_description, m_description.GetLength(), textRect, DT_CALCRECT);
 	LPtoDP(dc, &textRect.BottomRight(), 1);
 	::ReleaseDC(m_hWnd, dc);
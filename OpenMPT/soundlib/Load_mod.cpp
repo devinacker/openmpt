devinacker/openmpt

commit fbe69401f638714d901a775f051318300d41122f

[Fix] MOD: ProTracker does not sanitize the sample loop length, which can lead to detuned chip samples if the loop length is longer than the actual sample (fixes MOD.shorttune2) [Mod] OpenMPT: Version is now 1.26.00.27
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk@6331 56274372-70c3-4bfc-bfc3-4c3a0b034d27

OpenMPT/soundlib/Load_mod.cpp
@@ -261,10 +261,6 @@ struct PACKED MODSampleHeader
 			{
 				mptSmp.nLoopStart = mptSmp.nLength - 1;
 			}
-			if(mptSmp.nLoopEnd > mptSmp.nLength)
-			{
-				mptSmp.nLoopEnd = mptSmp.nLength;
-			}
 			if(mptSmp.nLoopStart > mptSmp.nLoopEnd || mptSmp.nLoopEnd < 4 || mptSmp.nLoopEnd - mptSmp.nLoopStart < 4)
 			{
 				mptSmp.nLoopStart = 0;
@@ -893,12 +889,20 @@ bool CSoundFile::ReadMod(FileReader &file, ModLoadingFlags loadFlags)
 	{
 		for(SAMPLEINDEX smp = 1; smp <= 31; smp++) if(Samples[smp].nLength)
 		{
+			SampleIO::Encoding encoding = file.ReadMagic("ADPCM") ? SampleIO::ADPCM : SampleIO::signedPCM;
+			// Fix sample 6 in MOD.shorttune2, which has a replen longer than the sample itself.
+			// ProTracker reads beyond the end of the sample when playing, so we just extend the sample with zeros.
+			// ReadSample will automatically do this for us if the sample chunk itself is too short.
+			FileReader sample = file.ReadChunk(Samples[smp].nLength);
+			if(isMdKd)
+				Samples[smp].nLength = std::max(Samples[smp].nLength, Samples[smp].nLoopEnd);
+
 			SampleIO(
 				SampleIO::_8bit,
 				SampleIO::mono,
 				SampleIO::littleEndian,
-				file.ReadMagic("ADPCM") ? SampleIO::ADPCM : SampleIO::signedPCM)
-				.ReadSample(Samples[smp], file);
+				encoding)
+				.ReadSample(Samples[smp], sample);
 		}
 	}
 

OpenMPT/common/versionNumber.h
@@ -19,7 +19,7 @@ OPENMPT_NAMESPACE_BEGIN
 #define VER_MAJORMAJOR          1
 #define VER_MAJOR               26
 #define VER_MINOR               00
-#define VER_MINORMINOR          26
+#define VER_MINORMINOR          27
 
 //Version string. For example "1.17.02.28"
 #define MPT_VERSION_STR         VER_STRINGIZE(VER_MAJORMAJOR) "." VER_STRINGIZE(VER_MAJOR) "." VER_STRINGIZE(VER_MINOR) "." VER_STRINGIZE(VER_MINORMINOR)

commit 296f585e4b984020ec183df43c0daa546bddc63a

[Fix] MOD: ADPCM-compressed samples broke in r6331 (https://bugs.openmpt.org/view.php?id=832).
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk@6662 56274372-70c3-4bfc-bfc3-4c3a0b034d27

OpenMPT/soundlib/Load_mod.cpp
@@ -897,22 +897,28 @@ bool CSoundFile::ReadMod(FileReader &file, ModLoadingFlags loadFlags)
 	// Reading samples
 	if(loadFlags & loadSampleData)
 	{
-		for(SAMPLEINDEX smp = 1; smp <= 31; smp++) if(Samples[smp].nLength)
+		for(SAMPLEINDEX smp = 1; smp <= 31; smp++)
 		{
-			SampleIO::Encoding encoding = file.ReadMagic("ADPCM") ? SampleIO::ADPCM : SampleIO::signedPCM;
-			// Fix sample 6 in MOD.shorttune2, which has a replen longer than the sample itself.
-			// ProTracker reads beyond the end of the sample when playing, so we just extend the sample with zeros.
-			// ReadSample will automatically do this for us if the sample chunk itself is too short.
-			FileReader sample = file.ReadChunk(Samples[smp].nLength);
-			if(isMdKd)
-				Samples[smp].nLength = std::max(Samples[smp].nLength, Samples[smp].nLoopEnd);
+			ModSample &sample = Samples[smp];
+			if(sample.nLength)
+			{
+				SampleIO::Encoding encoding = file.ReadMagic("ADPCM") ? SampleIO::ADPCM : SampleIO::signedPCM;
 
-			SampleIO(
-				SampleIO::_8bit,
-				SampleIO::mono,
-				SampleIO::littleEndian,
-				encoding)
-				.ReadSample(Samples[smp], sample);
+				SampleIO sampleIO(
+					SampleIO::_8bit,
+					SampleIO::mono,
+					SampleIO::littleEndian,
+					encoding);
+
+				// Fix sample 6 in MOD.shorttune2, which has a replen longer than the sample itself.
+				// ProTracker reads beyond the end of the sample when playing, so we just extend the sample with zeros.
+				// ReadSample will automatically do this for us if the sample chunk itself is too short.
+				FileReader sampleChunk = file.ReadChunk(sampleIO.CalculateEncodedSize(sample.nLength));
+				if(isMdKd)
+					sample.nLength = std::max(sample.nLength, sample.nLoopEnd);
+
+				sampleIO.ReadSample(sample, sampleChunk);
+			}
 		}
 	}
 
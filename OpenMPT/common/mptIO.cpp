devinacker/openmpt

commit 0a8abd08a8fd8fe68eef5a42e9c2ddcd988d6898

[Fix] FileReader::CanRead could overflow with a very long length (e.g. INT_MAX) and claim that the length is readable since r2002.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk@7166 56274372-70c3-4bfc-bfc3-4c3a0b034d27

OpenMPT/common/mptIO.h
@@ -520,7 +520,7 @@ public:
 
 	virtual bool CanRead(off_t pos, off_t length) const
 	{
-		return pos + length <= GetLength();
+		return length <= GetLength() - pos;
 	}
 
 	virtual off_t GetReadableLength(off_t pos, off_t length) const
@@ -598,7 +598,7 @@ public:
 		return data->Read(dst, dataOffset + pos, std::min(count, dataLength - pos));
 	}
 	bool CanRead(off_t pos, off_t length) const {
-		return (pos + length <= dataLength);
+		return (length <= dataLength - pos);
 	}
 	off_t GetReadableLength(off_t pos, off_t length) const
 	{
@@ -836,7 +836,7 @@ public:
 
 	bool CanRead(off_t pos, off_t length) const
 	{
-		return pos + length <= streamLength;
+		return pos < streamLength && length <= streamLength - pos;
 	}
 
 	off_t GetReadableLength(off_t pos, off_t length) const

commit 067067b59b1638e9058489510f4b214948c62cc3

[Fix] mptIO: Guard against post-eof positioning in IFileDataContainer::CanRead(). This can happen in particular for on-disk files that might change size while reading. Broken since r7166. [Fix] mptIO: Fix race against file size change in IFileDataContainer::GetReadableLength(). [Fix] mptIO: Fix integer overflows in FileDataContainerUnseekable::CacheStreamUpTo() and callers.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk@7169 56274372-70c3-4bfc-bfc3-4c3a0b034d27

OpenMPT/common/mptIO.cpp
@@ -518,17 +518,22 @@ void FileDataContainerUnseekable::CacheStream() const
 	streamFullyCached = true;
 }
 
-void FileDataContainerUnseekable::CacheStreamUpTo(std::streampos pos) const
+void FileDataContainerUnseekable::CacheStreamUpTo(off_t pos, off_t length) const
 {
 	if(streamFullyCached)
 	{
 		return;
 	}
-	if(pos <= std::streampos(cachesize))
+	if(length > std::numeric_limits<off_t>::max() - pos)
+	{
+		length = std::numeric_limits<off_t>::max() - pos;
+	}
+	std::size_t target = mpt::saturate_cast<std::size_t>(pos + length);
+	if(target <= cachesize)
 	{
 		return;
 	}
-	std::size_t alignedpos = Util::AlignUp<std::size_t>(static_cast<std::size_t>(pos), QUANTUM_SIZE);
+	std::size_t alignedpos = Util::AlignUp<std::size_t>(target, QUANTUM_SIZE);
 	std::size_t needcount = alignedpos - cachesize;
 	EnsureCacheBuffer(needcount);
 	std::size_t readcount = InternalRead(&cache[cachesize], alignedpos - cachesize);
@@ -570,7 +575,7 @@ IFileDataContainer::off_t FileDataContainerUnseekable::GetLength() const
 
 IFileDataContainer::off_t FileDataContainerUnseekable::Read(mpt::byte *dst, IFileDataContainer::off_t pos, IFileDataContainer::off_t count) const
 {
-	CacheStreamUpTo(pos + count);
+	CacheStreamUpTo(pos, count);
 	if(pos >= IFileDataContainer::off_t(cachesize))
 	{
 		return 0;
@@ -582,13 +587,21 @@ IFileDataContainer::off_t FileDataContainerUnseekable::Read(mpt::byte *dst, IFil
 
 bool FileDataContainerUnseekable::CanRead(IFileDataContainer::off_t pos, IFileDataContainer::off_t length) const
 {
-	CacheStreamUpTo(pos + length);
-	return pos + length <= IFileDataContainer::off_t(cachesize);
+	CacheStreamUpTo(pos, length);
+	if((pos == IFileDataContainer::off_t(cachesize)) && (length == 0))
+	{
+		return true;
+	}
+	if(pos >= IFileDataContainer::off_t(cachesize))
+	{
+		return false;
+	}
+	return length <= IFileDataContainer::off_t(cachesize) - pos;
 }
 
 IFileDataContainer::off_t FileDataContainerUnseekable::GetReadableLength(IFileDataContainer::off_t pos, IFileDataContainer::off_t length) const
 {
-	CacheStreamUpTo(pos + length);
+	CacheStreamUpTo(pos, length);
 	if(pos >= cachesize)
 	{
 		return 0;

OpenMPT/common/mptIO.h
@@ -520,16 +520,26 @@ public:
 
 	virtual bool CanRead(off_t pos, off_t length) const
 	{
-		return length <= GetLength() - pos;
+		off_t dataLength = GetLength();
+		if((pos == dataLength) && (length == 0))
+		{
+			return true;
+		}
+		if(pos >= dataLength)
+		{
+			return false;
+		}
+		return length <= dataLength - pos;
 	}
 
 	virtual off_t GetReadableLength(off_t pos, off_t length) const
 	{
-		if(pos >= GetLength())
+		off_t dataLength = GetLength();
+		if(pos >= dataLength)
 		{
 			return 0;
 		}
-		return std::min<off_t>(length, GetLength() - pos);
+		return std::min<off_t>(length, dataLength - pos);
 	}
 };
 
@@ -598,6 +608,14 @@ public:
 		return data->Read(dst, dataOffset + pos, std::min(count, dataLength - pos));
 	}
 	bool CanRead(off_t pos, off_t length) const {
+		if((pos == dataLength) && (length == 0))
+		{
+			return true;
+		}
+		if(pos >= dataLength)
+		{
+			return false;
+		}
 		return (length <= dataLength - pos);
 	}
 	off_t GetReadableLength(off_t pos, off_t length) const
@@ -685,7 +703,7 @@ private:
 
 	void EnsureCacheBuffer(std::size_t requiredbuffersize) const;
 	void CacheStream() const;
-	void CacheStreamUpTo(std::streampos pos) const;
+	void CacheStreamUpTo(off_t pos, off_t length) const;
 
 private:
 
@@ -836,7 +854,15 @@ public:
 
 	bool CanRead(off_t pos, off_t length) const
 	{
-		return pos < streamLength && length <= streamLength - pos;
+		if((pos == streamLength) && (length == 0))
+		{
+			return true;
+		}
+		if(pos >= streamLength)
+		{
+			return false;
+		}
+		return (length <= streamLength - pos);
 	}
 
 	off_t GetReadableLength(off_t pos, off_t length) const